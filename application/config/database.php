b8f0288140c02bf625390a7ad509be36d700c7b7
Shaun Djuhari <shaundjuhari@berkeley.edu>
2017-06-03 16:50:32 +0800

    try db

--- application/config/database.php
+++ application/config/database.php
@@ -4,12 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+mysql://bd19b211e7f6a8:42d64e75@us-cdbr-iron-east-03.cleardb.net/heroku_4b8db98f9951299?reconnect=true
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'restaurant',
+	'hostname' => 'us-cdbr-iron-east-03.cleardb.net',
+	'username' => 'bd19b211e7f6a8',
+	'password' => '42d64e75',
+	'database' => 'heroku_4b8db98f9951299',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
